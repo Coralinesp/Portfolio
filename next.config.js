bcb1beb3783312b2ae3d13e909bcc83cffdb9d39
Coralinesp <perezcruzcaroline@gmail.com>
2025-08-05 19:56:36 -0400

    .

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const repo = 'portfolio' // o el nombre real de tu repo
+const repo = 'portfolio' // nombre de tu repo en GitHub, todo en minúsculas
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: `/${repo}`,
   assetPrefix: `/${repo}/`,
-  distDir: 'docs', // <--- esto hace que se genere /docs en lugar de /out
+  distDir: 'docs', // carpeta de salida para el export
 }
 
 module.exports = nextConfig;
